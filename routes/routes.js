8cc135b6046fc98720ecd3e41425f8e39344e363
Shahid <shahid.sarker@gmail.com>
2020-10-22 12:28:22 -0400

    Set up login route with JWT

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -12,5 +14,27 @@ router.post(
     res.json({ message: "Signup successful", user: req.user });
   }
 );
+
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("An error occured.");
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, email: user.email };
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
+
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
