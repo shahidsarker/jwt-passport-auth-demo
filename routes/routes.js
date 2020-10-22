bdf68d7bc5afff901acc229fc8750f5fe60db06a
Shahid <shahid.sarker@gmail.com>
2020-10-22 12:20:47 -0400

    signup route

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+module.exports = router;
+
+router.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
+    res.json({ message: "Signup successful", user: req.user });
+  }
+);
+
+module.exports = router;
